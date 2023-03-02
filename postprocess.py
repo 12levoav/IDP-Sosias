1ed69065e92dffd75ad3e1cab7f08e91769e6acc
Levon <12levoav@gmail.com>
2023-03-02 18:07:57 +0400

    final post processing rule

--- postprocess.py
+++ postprocess.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 
 
@@ -53,20 +52,102 @@ def group_data(cleared_data, median):
     return group
 
 
+def find_nearest(array, number):
+    nearest_index = 0
+    smallest_difference = abs(number - array[0])
+
+    for i in range(1, len(array)):
+        current_difference = abs(number - array[i])
+
+        if current_difference < smallest_difference:
+            smallest_difference = current_difference
+            nearest_index = i
+
+    return nearest_index
+
+
+def sync_per_name(group):
+    if "PER-NAME" in group and len(group["PER-NAME"]) > 1:
+        names_object = group["PER-NAME"]
+        names = []
+        for key in names_object:
+            for s in range(len(names_object[key])):
+                names.append(names_object[key][s])
+        return {1: names}
+
+
+def sync_comp_name(group):
+    if "COMP-NAME" in group and len(group["COMP-NAME"]) > 1:
+        names_object = group["COMP-NAME"]
+        names = []
+        for key in names_object:
+            for s in range(len(names_object[key])):
+                names.append(names_object[key][s])
+        return {1: names}
+
+
+def get_rows(group):
+    rows = []
+    if "PER-NAME" in group:
+        names = group["PER-NAME"]
+        for key in names:
+            middle = []
+            row_arr = names[key]
+            for i in range(len(row_arr)):
+                middle.append(row_arr[i]["y_middle"])
+            rows.append(sum(middle) / len(middle))
+
+    if "COMP-NAME" in group:
+        names = group["COMP-NAME"]
+        for key in names:
+            middle = []
+            row_arr = names[key]
+            for i in range(len(row_arr)):
+                middle.append(row_arr[i]["y_middle"])
+            rows.append(sum(middle) / len(middle))
+
+    return rows
+
+
 def sync_names(groups):
     clean_groups = groups
     for z in range(len(clean_groups)):
         group = clean_groups[z]
-        if "PER-BIRTHDAY" in group and len(group["PER-BIRTHDAY"]) == 1:
-            if "PER-NAME" in group and len(group["PER-NAME"]) > 1:
-                names_object = group["PER-NAME"]
-                names = []
-                for key in names_object:
-                    for s in range(len(names_object[key])):
-                        names.append(names_object[key][s])
-                group["PER-NAME"] = {1: names}
+        if ("PER-BIRTHDAY" in group and len(group["PER-BIRTHDAY"]) == 1) or (
+                "PER-CITY" in group and len(group["PER-CITY"]) == 1):
+            group["PER-NAME"] = sync_per_name(group)
+        if ("COMP-CITY" in group and (len(group["COMP-CITY"]) == 1)) or (
+                "COMP-DISTRICT_COURT" in group and (len(group["COMP-DISTRICT_COURT"]) == 1)) or (
+                "COMP-ID" in group and (len(group["COMP-ID"]) == 1)):
+            group["COMP-NAME"] = sync_comp_name(group)
         clean_groups[z] = group
     return clean_groups
+
+def update_group(group, rows):
+    for key in group:
+        new_rows = rows.copy()
+
+        if len(group[key]) > len(rows):
+            new_obj = {}
+            for inner_key in group[key]:
+                inner_value = sorted(group[key][inner_key], key=lambda x: x['y_middle'], reverse=True)
+                closest = find_nearest(new_rows, inner_value[0]['y_middle'])
+                if inner_value[0]['y_middle'] > new_rows[closest] or closest == 0:
+                    new_rows[closest] = inner_value[0]['y_middle']
+                    if closest + 1 not in new_obj:
+                        new_obj[closest + 1] = inner_value
+                    else:
+                        new_obj[closest + 1] = inner_value + new_obj[closest + 1]
+                else:
+                    new_rows[closest - 1] = inner_value[0]['y_middle']
+                    if closest not in new_obj:
+                        new_obj[closest] = inner_value
+                    else:
+                        new_obj[closest] = inner_value + new_obj[closest]
+
+            group[key] = new_obj
+
+    return group
 
 
 if __name__ == '__main__':
@@ -77,10 +158,9 @@ if __name__ == '__main__':
         median = calculate_median(x['output'])
         groups.append(group_data(x['output'], median))
     groups = sync_names(groups)
+    for group in groups:
+        rows = get_rows(group)
+        group.update(update_group(group, rows))
     inference_out = [json.dumps(groups, ensure_ascii=False)][0]
     with open('Cleaned_Group.json', 'w') as inf_out:
         inf_out.write(inference_out)
-
-
-
-
